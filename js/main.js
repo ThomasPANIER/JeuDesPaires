927718376533f570c0a17084cee9bd1250f4a974
ThomasPANIER <thomaspanier.pro@gmail.com>
2021-04-15 11:37:51 +0200

    add transition to flipCard

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
+// Récupérer les éléments html card sous forme de tableau.
 const cards = document.querySelectorAll(".card");
 
-cards.forEach(card => addEventListener("click", flipCard));
+// La méthode forEach() permet d'exécuter une fonction donnée sur chaque élément du tableau.
+  // on ajoute ensuite aux éléments un écouteur d'événements qui sera un click sur une card pour y ajouter la fonction. 
+cards.forEach(card => card.addEventListener("click", flipCard));
 
+// Fonction flipCard() déclenchée par foreach() permet d'accéder à la classe de card avec la méthode toggle.
+  // Utilisation de la méthode toggle pour cacher ou montrer un élément du dom.
 function flipCard() {
+  //cards.classList.add("flip");
   console.log(this);
-  console.log("C'est cliqué");
+  this.classList.toggle("flip");
 }
 
-// console.log(cards);
+
+
+
+
+
+
+
+
+
+
 
 // const colors = ["red", "green", "yellow", "blue", "purple", "pink", "red", "green", "yellow", "blue", "purple", "pink"];
 
