dfe564dc3aea6711eca61add6e822dcbdb805564
ThomasPANIER <thomaspanier.pro@gmail.com>
2021-04-16 15:07:56 +0200

    add function compareCard

--- js/main.js
+++ js/main.js
@@ -1,19 +1,88 @@
+
 // Récupérer les éléments html card sous forme de tableau.
-const cards = document.querySelectorAll(".card");
+const cards = document.querySelectorAll(".gameCard");
+//console.log();
 
 // La méthode forEach() permet d'exécuter une fonction donnée sur chaque élément du tableau.
   // on ajoute ensuite aux éléments un écouteur d'événements qui sera un click sur une card pour y ajouter la fonction. 
 cards.forEach(card => card.addEventListener("click", flipCard));
+// for (card of cards) {
+//   card = card.addEventListener("click", flipCard);
+//   //console.log();
+// }
 
-// Fonction flipCard() déclenchée par foreach() permet d'accéder à la classe de card avec la méthode toggle.
+
+
+//par default la class "flip" n'est pas mise.
+//let addedFlipClass = false;
+
+// 1ere et 2eme card retournée.
+let cardOne;
+let cardTwo;
+let test = [];
+
+//let dataColor = document.querySelectorall('.card.flip');
+// console.log("dataColor");
+
+// Fonction flipCard() déclenchée par foreach() permet d'accéder à la classe de card pour appliquer la méthode toggle.
   // Utilisation de la méthode toggle pour cacher ou montrer un élément du dom.
 function flipCard() {
-  //cards.classList.add("flip");
+  //this.classList.toggle("flip");
+  this.classList.add("flip");
   console.log(this);
-  this.classList.toggle("flip");
+  test.push(this);
+  console.log(test);
+    if (test.length === 2) {
+      compareCard();
+      
+    }
+  
+//}  
+};
+
+//style = "background-color:blue";
+//test[0].style.backgroundColor === ....
+
+function compareCard() {
+  if (test[0].dataset.name === test[1].dataset.name) {
+    alert("identique");
+    console.log(test[0].dataset.name);
+  }
+  else {
+    test[0].classList.remove("flip");
+    test[1].classList.remove("flip");
+  }
+  test = [];
 }
 
 
+// Déterminer si l'utilisateur click sur la 1ere ou 2eme card et comparer les choix
+  
+  
+//   if (!addedFlipClass) {
+//     addedflipClass = true;
+//     cardOne = this;
+//   }
+//   else {
+//     addedFlipClass = false;
+//     cardTwo = this;
+  
+
+  
+  
+
+//     if (cardOne.dataset.color === cardTwo.dataset.color) {
+//       cardOne.removeEventListener("click", flipCard);
+//       cardTwo.removeEventListener("click", flipCard);
+//     }
+//     else {
+//       cardOne.classList.remove("flip");
+//       cardTwo.classList.remove("flip");
+//     }
+//     console.log(cardTwo.dataset.color);
+//   }
+//   console.log([addedFlipClass, cardOne]);
+// }
 
 
 
