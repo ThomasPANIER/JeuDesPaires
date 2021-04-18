de5682c1976dd3fb91eca5b9dd4719d499f359a5
ThomasPANIER <thomaspanier.pro@gmail.com>
2021-04-18 15:57:23 +0200

    add comment, logo and icon

--- js/main.js
+++ js/main.js
@@ -4,20 +4,19 @@ const sections = document.querySelectorAll(".col-3");
 // Récupérer les éléments html card sous forme de tableau.
 const cards = document.querySelectorAll(".gameCard");
 
-// La méthode forEach() permet d'exécuter une fonction donnée sur chaque élément du tableau.
-  // on ajoute ensuite aux éléments un écouteur d'événements qui sera un click sur une card pour y ajouter la fonction. 
+// La méthode forEach() permet d'exécuter une fonction sur chaque élément du tableau.
+  // on ajoute ensuite aux éléments un écouteur d'événements, ici un click sur une card pour y ajouter la fonction. 
 cards.forEach(card => card.addEventListener("click", flipCard));
 // Même finalité que la fonction fléché.
   // for (card of cards) {
   //   card = card.addEventListener("click", flipCard);
-  //   //console.log();
   // }
 
 // Stocker les choix de l'utilisateur pour la 1ere et 2eme card et comparer les choix.
   // Les cartes retournées sont stocker dans un tableau vide.
 let choiceCard = [];
 
-// Fonction flipCard() déclenchée par foreach() permet d'accéder à l'élément card pour appliquer la méthode add.
+// Fonction flipCard() appliquée par foreach() permet d'accéder à l'élément card pour appliquer la méthode add.
 function flipCard() {
   // Ajout de la classe "flip" à l'élément card.
   this.classList.add("flip");
@@ -26,7 +25,7 @@ function flipCard() {
   // Vérifier qu'il y a bien 2 card dans le tableau pour pouvoir comparer.
     // si plus de 2 card choisi lancer une alert et retourner les card.
     if  (choiceCard.length > 2) {
-      alert("trop c'est trop !");
+      alert("  Trop c'est trop ! \n Il ne faut retouner \n     que 2 cartes.");
       choiceCard[2].classList.remove("flip");
     }
     // si le bon nombre de card est choisi lancer la comparaison.  
@@ -36,7 +35,7 @@ function flipCard() {
     }  
 };
 
-// Fonction compareCard pour identifier les cartes avec data-name et comparer si identique.
+// Fonction compareCard pour identifier les card avec data-name et comparer si identique.
 function compareCard() {
   if (choiceCard[0].dataset.name === choiceCard[1].dataset.name) {
     // si toutes les paires sont trouvées, proposition de recommencer le jeu.
@@ -48,7 +47,6 @@ function compareCard() {
 
     // else {
     // win();
-    // console.log(choiceCard[0].dataset.name);
     // }  
   }
   // si choix pas identiques enlève la classe "flip" pour cacher de nouveau les card.
