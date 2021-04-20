8cbec3d57b805253bfc4e66ecd5d80d38d6d6c21
ThomasPANIER <thomaspanier.pro@gmail.com>
2021-04-20 14:45:14 +0200

    bug fix when click again on a pair

--- js/main.js
+++ js/main.js
@@ -39,7 +39,11 @@ function compareCard() {
     choiceCard[1].classList.remove("flip");
     choiceCard = [];
   }
+  // one pair found.
   else if (choiceCard[0].dataset.name === choiceCard[1].dataset.name) {
+    // we remove eventListener to not be able to reclick on the pair.
+    choiceCard[0].removeEventListener("click", flipCard);
+    choiceCard[1].removeEventListener("click", flipCard);
     // if all the pairs are found, offer to restart the game.
     let winningPair = document.querySelectorAll(".flip");
     if (winningPair.length === 12) {
